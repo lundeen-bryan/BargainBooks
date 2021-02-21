ee9a805acea81c0ae3a8e5a707ef4b753e296e2c
lundeen-bryan <lundeen.bryan@gmail.com>
2021-02-20 16:42:36 -0800

    Added jquery anim to lte box & media ques

--- js/script.js
+++ js/script.js
@@ -1,34 +1,15 @@
+$(document).ready(function () {
+  lightbox.option({
+    resizeDuration: 300,
+    wrapAround: true /* removed for spead sake 'resizeDuration': 200, */,
+  });
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (position >= 350) {
+      $('.gallery').addClass('change');
+    } else {
+      $('.gallery').removeClass('change');
+    }
+  });
+});
